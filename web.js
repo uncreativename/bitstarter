501171cfa495680031d99434838d4afb7ed3f40a
Ubuntu <ubuntu@ip-172-31-21-169.us-west-2.compute.internal>
2013-07-11 00:35:03 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,10 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer("Im as string", "utf-8");
+fs.readFile('/home/ubuntu/bitstarter/index.html', function(err,data) ) 
+{
+	if (err) throw err;
+	console.log(data);
+});
+
+var buffer = new Buffer(data, "utf-8");
 
 app.get('/', function(request, response) {
-  response.send("buffer.toString");
+  response.send(buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
