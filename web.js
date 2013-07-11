70150945d5847cfe4b623659a4f4798d5016a806
Ubuntu <ubuntu@ip-172-31-21-169.us-west-2.compute.internal>
2013-07-11 00:50:42 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,11 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//fs.readFile('/home/ubuntu/bitstarter/index.html', function(err,data) ) 
-//{
-//	if (err) throw err;
-//	console.log(data);
-//});
+fs.readFile('/home/ubuntu/bitstarter/index.html', function(err,data) ) 
+{
+	if (err) throw err;
+	console.log(data);
+});
 
 var buffer = new Buffer("testing2", "utf-8");
 
